5e01dbeab16facef6a9dbc8a7836e9bd227900df
knosko <peter.knosko@gmail.com>
2023-08-28 18:37:27 +0200

    Skusanie cyklu for pre scitanie vsetkych cisiel v poli

---
+++ src/Polia.java
@@ -0,0 +1,16 @@
+public class Polia {
+    public static void main(String[] args) {
+
+        int[] cisla = {4,5,9,12,6};
+        int sucet = 0;
+        for (int i=0; i < cisla.length; i++) {
+            sucet = sucet + cisla[i];
+
+
+        }
+        System.out.println("Súčet vsetkych cisel v poli je: " +sucet);
+
+    }
+
+
+}
