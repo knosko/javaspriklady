8555cb049d70378d4acb89e0fcac3cf601afe484
knosko <peter.knosko@gmail.com>
2023-08-28 19:52:52 +0200

    Doplnenie metody na vypocet poctu cisiel a najdenia najvacsieho cisla v poli

--- src/Polia.java
+++ src/Polia.java
@@ -18,12 +18,28 @@ public class Polia {
         return priemer;
     }
 
+    public static int pocetCiselaMax (int[] cisla) {
+        int maxCislo = cisla[0];
+        int pocetCisel = 0;
+        for (int i = 0; i < cisla.length; i++) {
+            pocetCisel = pocetCisel + 1;
+            if (cisla[i] > maxCislo) {
+                maxCislo = cisla[i];
+            }
+        }
+        System.out.println("Počet čísiel v poli je: " + pocetCisel);
+        return maxCislo;
+    }
+
     public static void main(String[] args) {
         int[] poleCisiel = {4, 5, 9, 12, 6};
         int sucet = spocitaj(poleCisiel);
         double priemer = priemeruj(poleCisiel);
+        int maxCislo = pocetCiselaMax(poleCisiel);
+        //int pocetCisel = pocetCiselaMax(poleCisiel);
         System.out.println("Súčet vsetkych cisel v poli je: " + sucet);
         System.out.println("Priemer vsetkych cisel v poli je: " + priemer);
+        System.out.println("Maximálne cislo v poli je: " + maxCislo);
     }
 }
 
