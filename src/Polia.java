40d6eb739ab8de23b69e884586accfc4aa88902f
knosko <peter.knosko@gmail.com>
2023-08-28 19:32:27 +0200

    Prerobenie programu na metody a doplnenie metody na priemer

--- src/Polia.java
+++ src/Polia.java
@@ -1,16 +1,29 @@
 public class Polia {
-    public static void main(String[] args) {
-
-        int[] cisla = {4,5,9,12,6};
+    public static int spocitaj (int[] cisla){
         int sucet = 0;
-        for (int i=0; i < cisla.length; i++) {
+        for (int i = 0; i < cisla.length; i++) {
             sucet = sucet + cisla[i];
-
-
         }
-        System.out.println("Súčet vsetkych cisel v poli je: " +sucet);
-
+        return sucet;
     }
 
+    public static double priemeruj (int[] cisla){
+        int sucet = 0;
+        int pocetCisel  = 0;
+        for (int i = 0; i < cisla.length; i++) {
+            pocetCisel= pocetCisel + 1;
+            sucet = sucet + cisla[i];
+        }
+        double priemer = (double) sucet / pocetCisel;
+        return priemer;
+    }
 
+    public static void main(String[] args) {
+        int[] poleCisiel = {4, 5, 9, 12, 6};
+        int sucet = spocitaj(poleCisiel);
+        double priemer = priemeruj(poleCisiel);
+        System.out.println("Súčet vsetkych cisel v poli je: " + sucet);
+        System.out.println("Priemer vsetkych cisel v poli je: " + priemer);
+    }
 }
+
